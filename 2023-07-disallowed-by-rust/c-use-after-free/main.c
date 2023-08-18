e1f91ac81b0116f2565185d12ceed8941b8deb0c
Tim McNamara <code@timmcnamara.co.nz>
2023-08-18 12:13:36 +1200

    feat: add disallowed by rust

--- 2023-07-disallowed-by-rust/c-use-after-free/main.c
+++ 2023-07-disallowed-by-rust/c-use-after-free/main.c
@@ -4,6 +4,8 @@ int main() {
     int *p = (int *) malloc(sizeof(int));
     *p = 10;
     free(p);
+
     *p = 20;
+    
     return 0;
 }
---
+++ 2023-07-disallowed-by-rust/nullptr
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cd java-null-ptr
+java Main
